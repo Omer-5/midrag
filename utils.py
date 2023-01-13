e708158224927f7ff040217c99ecc00207810946
Omer-5 <OmerHazan5@gmail.com>
2023-01-13 21:10:21 +0200

    make utils file

---
+++ utils.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def Save_CSV(CSV_name, df:pd.DataFrame):
+    df.to_csv(f"data/{CSV_name}", index=False, encoding = 'utf-8-sig')
+
+def Append_Or_Create_CSV_By_Name(CSV_name, df:pd.DataFrame):
+    output_path=f'data/{CSV_name}'
+    df.to_csv(output_path, mode='a', index =False, header=not os.path.exists(output_path), encoding = 'utf-8-sig')
