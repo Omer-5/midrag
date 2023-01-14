29a30551e5da661ef192c0039c1fe091cc7f6d85
Omer-5 <OmerHazan5@gmail.com>
2023-01-14 13:13:30 +0200

    move to utils

--- utils.py
+++ utils.py
@@ -7,3 +7,9 @@ def Save_CSV(CSV_name, df:pd.DataFrame):
 def Append_Or_Create_CSV_By_Name(CSV_name, df:pd.DataFrame):
     output_path=f'data/{CSV_name}'
     df.to_csv(output_path, mode='a', index =False, header=not os.path.exists(output_path), encoding = 'utf-8-sig')
+
+def Sort_Reviews_CSV():
+    filename = 'reviews.csv'
+    df_reviews = pd.read_csv(f"data/{filename}")
+    df_reviews.sort_values(by=['Category', 'Subcategory', 'Date'],inplace=True, ascending=[True, True, False])
+    Save_CSV(filename, df_reviews)
