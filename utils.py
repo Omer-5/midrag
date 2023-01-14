976276c61099853a590fca98a410a13d612af7a7
Daniel Ventura <106916910+smooth3x@users.noreply.github.com>
2023-01-14 13:47:11 +0200

    Added 'Split_Subcategories' method

--- utils.py
+++ utils.py
@@ -7,3 +7,14 @@ def Save_CSV(CSV_name, df:pd.DataFrame):
 def Append_Or_Create_CSV_By_Name(CSV_name, df:pd.DataFrame):
     output_path=f'data/{CSV_name}'
     df.to_csv(output_path, mode='a', index =False, header=not os.path.exists(output_path), encoding = 'utf-8-sig')
+
+def Split_SubCategories():
+    # Split all_subcateogries file into two
+
+    df = pd.read_csv("data\\all_subcategories_URLs.csv")
+
+    df1 = df.iloc[:int(len(df)*0.5)]
+    df2 = df.iloc[int(len(df)*0.5):]
+
+    Save_CSV("all_subcategories_1.csv", df1)
+    Save_CSV("all_subcategories_2.csv", df2)
