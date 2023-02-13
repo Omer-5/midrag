dc0dd480d095246a1ebc8b1d6ecaa7e407540ce5
Omer-5 <OmerHazan5@gmail.com>
2023-02-13 22:26:21 +0200

    docostring

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import pandas as pd
 import os
 
 def Save_CSV(CSV_name, df:pd.DataFrame):
+    """Save CSV from DataFrame Object
+
+    Args:
+        CSV_name (string): include the .csv at the end
+        df (pd.DataFrame): Pandas.DataFrame
+    """
     df.to_csv(f"data/{CSV_name}", index=False, encoding = 'utf-8-sig')
 
 def Append_Or_Create_CSV_By_Name(CSV_name, df:pd.DataFrame):
